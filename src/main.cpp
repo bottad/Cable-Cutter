09569516bde82c607d4a15e260498d932f3e0009
bottad <david.botta@gmx.ch>
2023-08-21 19:09:43 +0200

    initialized programming environnement

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "functions.hpp"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  
+  int c = add(4,5);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
